46109fc2026938b3e57db0119041bef1ba37f59e
iwantacrepe <100364726+iwantacrepe@users.noreply.github.com>
2024-08-01 22:58:23 +0530

    Update settings.py

--- Forum/settings.py
+++ Forum/settings.py
@@ -66,18 +66,13 @@ WSGI_APPLICATION = 'Forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-}
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
 
 
 # Password validation

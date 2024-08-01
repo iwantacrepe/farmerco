55609f049f90fd95f4c00a4468df22e89666b623
iwantacrepe <100364726+iwantacrepe@users.noreply.github.com>
2024-08-01 23:03:29 +0530

    Update settings.py

--- Forum/settings.py
+++ Forum/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'Forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3',
